1b6af26d6c2e8b599e8e2976ba4628ac0d7b74c9
Daniel Fich <danielfich2@gmail.com>
2024-04-19 22:33:58 +0200

    Bullet bounces

---
+++ Entities/Bullet/Bullet.gd
@@ -0,0 +1,14 @@
+extends AnimatableBody2D
+
+var velocity: Vector2 = Vector2(-5, 5)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	var collision = move_and_collide(velocity)
+
+	if collision != null:
+		velocity = get_vector_reflection(velocity, collision.get_normal())
+
+func get_vector_reflection(vector: Vector2, normal: Vector2) -> Vector2:
+	return vector - 2 * vector.dot(normal) * normal
