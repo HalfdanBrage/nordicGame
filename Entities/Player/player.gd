6ff2a30d46f6b082e05e2bafc1cf2495008508e5
habr <habr@itu.dk>
2024-04-20 13:18:12 +0200

    refactored active bullet system

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -2,29 +2,28 @@ extends CharacterBody2D
 
 class_name Player
 
-const BULLET_PATH = "res://Entities/Bullet/bullet.tscn"
-var bullet = preload(BULLET_PATH)
+const BULLET_RES = preload("res://Entities/Bullet/bullet.tscn")
 
+var bullet = null
 
 var is_controller = false
 
 func _ready():
 	Levelmanager.set_player(self)
-	Levelmanager.add_exception_body(self)
 
 func _physics_process(delta):
 	pass	
 
 func _input(event):
 	if Input.is_action_just_pressed("swap"):
-		swap_positions(self, Levelmanager.get_newest_bullet())
+		swap()
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-	
+	# TODO: refine joypad aiming later
 	if event is InputEventJoypadButton:
 		is_controller = true
-	else:
+	elif event is InputEventMouse:
 		is_controller = false
 
 func get_aim_angle():
@@ -36,16 +35,31 @@ func get_aim_angle():
 	else:
 		return global_position.direction_to(get_global_mouse_position())
 
+func swap():
+	if bullet != null:
+		swap_positions(self, bullet)
+		disable_bullet()
+
 func swap_positions(node_1: Node2D, node_2: Node2D):
-	var tmp = Vector2(node_1.position)
-	node_1.position = node_2.position
-	node_2.position = tmp
+	var tmp = Vector2(node_1.global_position)
+	node_1.global_position = node_2.global_position
+	node_2.global_position = tmp
 
 func shoot():
-	var new_bullet = bullet.instantiate()
-	new_bullet.dir = get_aim_angle()
-	new_bullet.position = position
-	get_parent().add_child(new_bullet)
+	var aim_dir = get_aim_angle()
+	if aim_dir:
+		disable_bullet()
+		var new_bullet = BULLET_RES.instantiate()
+		new_bullet.dir = aim_dir
+		new_bullet.position = position
+		bullet = new_bullet
+		get_parent().add_child(new_bullet)
+		new_bullet.activate()
+
+func disable_bullet():
+	if not bullet == null:
+		bullet.deactivate()
+		bullet = null
 
 func damage():
 	print("OWWW")
--- singletons/levelmanager.gd
+++ singletons/levelmanager.gd
@@ -1,23 +1,8 @@
 extends Node
 
-
-var _bullet: Bullet = null
 var _player: Player = null
-
 
 func set_player(player: Player):
 	_player = player
 func get_player() -> Player:
 	return _player
-func kill_player():
-	_player.queue_free()
-	_player = null
-
-func add_bullet(bullet: Bullet):
-	_bullet = bullet
-func get_newest_bullet() -> Bullet:
-	return _bullet
-
-func add_exception_body(node: Node2D):
-	if _bullet != null:
-		_bullet.add_collision_exception_with(node)
