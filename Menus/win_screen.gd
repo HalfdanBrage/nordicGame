e035fda2b20d4bbbe1e731f827881adaa4427e0d
habr <habr@itu.dk>
2024-04-20 21:08:39 +0200

    modified win screen

--- Menus/win_screen.gd
+++ Menus/win_screen.gd
@@ -1,4 +1,12 @@
 extends CanvasLayer
 
+@export var enable_dither = false
+
 func _ready():
 	Musicplayer.play_out()
+
+func _process(delta):
+	if enable_dither:
+		var gain = $TextureRect.texture.noise.fractal_gain
+		var new_gain = gain - gain * delta * 0.95
+		$TextureRect.texture.noise.fractal_gain = new_gain 
