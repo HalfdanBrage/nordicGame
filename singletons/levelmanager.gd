a1b5a74999c5bb45cdc3f059b857da6f82c33398
Henrik <henrik@schwrz.dk>
2024-04-20 10:20:45 +0200

    Added swapping

--- singletons/levelmanager.gd
+++ singletons/levelmanager.gd
@@ -1,6 +1,13 @@
 extends Node
 
 
-var _bullet
-var _player
+var _bullet: CharacterBody2D
+var _player: CharacterBody2D
+
+# Keep track of if the body just swapped
+var just_swapped = false
+
+func add_exception_body(node: Node2D):
+	if _bullet != null:
+		_bullet.add_collision_exception_with(node)
 
