88bd8f7c9e721743173b8a36696423ad186f83e2
Henrik <henrik@schwrz.dk>
2024-04-20 16:10:54 +0200

    added texturized play button

--- Menus/MainScreen/main.gd
+++ Menus/MainScreen/main.gd
@@ -2,14 +2,6 @@ extends MarginContainer
 
 
 
-
 func _on_play_pressed():
 	Levelmanager.select_level(0)
 	
-
-func _on_select_level_pressed():
-	pass # Replace with function body.
-
-
-func _on_quit_pressed():
-	get_tree().quit()
