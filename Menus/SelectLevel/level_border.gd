43d38798060492e2631057d29828c7e03de26bde
Henrik <henrik@schwrz.dk>
2024-04-21 13:59:19 +0200

    added credits screen

--- Menus/SelectLevel/level_border.gd
+++ Menus/SelectLevel/level_border.gd
@@ -13,11 +13,8 @@ func set_level(label: int, num: int):
 	$Texture_2_1.texture = load("res://Assets/UI/" + str(n1)+".png")
 	$Texture_2_2.texture = load("res://Assets/UI/" + str(n2)+".png")
 
-
-
 func _on_pressed():
 	Levelmanager.select_level(level_number)
-
 
 func _on_focus_entered():
 	for child in get_children():
